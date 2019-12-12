45f83fba2910731272174585995e3e56654b387e
Megan Vogler <mmphilmon@gmail.com>
2019-12-12 09:54:43 -0600

    added subtitle

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,6 +4,9 @@ import "./style.css";
 const Title = props => (
   <div className="header">
     <div className="title">{props.children}</div>
+    <div className="subtitle">
+      Click on an image to earn points, but don't click on any more than once!
+    </div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
